f4552ff53522cf8de9778fcec002205644f836c8
Maxime LECHERE <maximelechere.dev@gmail.com>
2023-05-11 23:28:53 +0200

    création des entités

---
+++ app/src/Entity/ExpenseNote.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace App\Entity;
+
+use ApiPlatform\Metadata\ApiResource;
+use App\Repository\ExpenseNoteRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: ExpenseNoteRepository::class)]
+#[ApiResource]
+class ExpenseNote
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(type: Types::DATE_MUTABLE)]
+    private ?\DateTimeInterface $noteDate = null;
+
+    #[ORM\Column(type: Types::DECIMAL, precision: 7, scale: 2)]
+    private ?string $amount = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $type = null;
+
+    #[ORM\Column(type: Types::DATE_MUTABLE)]
+    private ?\DateTimeInterface $registrationDate = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNoteDate(): ?\DateTimeInterface
+    {
+        return $this->noteDate;
+    }
+
+    public function setNoteDate(\DateTimeInterface $noteDate): self
+    {
+        $this->noteDate = $noteDate;
+
+        return $this;
+    }
+
+    public function getAmount(): ?string
+    {
+        return $this->amount;
+    }
+
+    public function setAmount(string $amount): self
+    {
+        $this->amount = $amount;
+
+        return $this;
+    }
+
+    public function getType(): ?string
+    {
+        return $this->type;
+    }
+
+    public function setType(string $type): self
+    {
+        $this->type = $type;
+
+        return $this;
+    }
+
+    public function getRegistrationDate(): ?\DateTimeInterface
+    {
+        return $this->registrationDate;
+    }
+
+    public function setRegistrationDate(\DateTimeInterface $registrationDate): self
+    {
+        $this->registrationDate = $registrationDate;
+
+        return $this;
+    }
+}
